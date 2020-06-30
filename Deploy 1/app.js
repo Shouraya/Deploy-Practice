7c087b92f08acc0e55062dee3e6515759f6c14b5
Shouraya <shourayagoyal2406@gmail.com>
2020-06-30 19:54:12 +0530

    added script start

--- Deploy 1/app.js
+++ Deploy 1/app.js
@@ -6,9 +6,11 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("home");
 });
+
 app.get("/about", function(req, res){
     res.render("about");
 });
+
 app.listen(3000, function(){
 	// console.log("Server has Started!");
 });
